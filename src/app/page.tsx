be586d2925d2ce5b208d0a97077c2de314c92c5f
Shashivadan <shashivadan99@gmail.com>
2024-08-18 18:16:18 +0530

    feat: some framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,41 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Header from "@/components/Header";
 import InfoCard from "@/components/InfoCard";
 
 export default function Home() {
   return (
     <>
-      <div className=" bg-primary-color text-white h-screen bg-black  overflow-hidden ">
-        <div
-          className="hidden md:block md:absolute   md:top-[-300px] md: left-[500px]  h-[1000px] w-[1000px] overflow-hidden"
+      <div className="bg-primary-color text-white h-screen bg-black overflow-hidden">
+        <motion.div
+          className="hidden md:block md:absolute md:top-[-300px] md:left-[500px] h-[1000px] w-[1000px] overflow-hidden"
           style={{
             backgroundImage:
               "radial-gradient(closest-side, rgba(128,0,128,0.21), #0000)",
           }}
-        ></div>
-        <div className="  relative h-full w-full">
-          <Header />
-          <div className="absolute top-40 w-full">
-            <div className=" flex justify-center">
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        ></motion.div>
+        <div className="relative h-full w-full">
+          <motion.div
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+          >
+            <Header />
+          </motion.div>
+          <motion.div
+            className="absolute top-40 w-full"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+          >
+            <div className="flex justify-center">
               <InfoCard />
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import { config } from "@/data/config";
 import Verfiy from "@/components/ui/svgs/Verfiy";
 import Cells from "./Cells";
 import Social from "./Social";
+import { motion } from "framer-motion";
 
 export default function InfoCard() {
   return (
     <div className="  w-full  md:h-96">
-      <div className="flex items-center justify-center mt-5">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        
+        transition={{ duration: 0.3, ease: "easeOut", delay: 0.2 }}
+        className="flex items-center justify-center mt-5"
+      >
         <Image
           src={config.avater}
           alt="avater"
@@ -17,7 +26,7 @@ export default function InfoCard() {
           width={100}
           height={100}
         />
-      </div>
+      </motion.div>
       <div className=" flex justify-center items-center gap-1">
         <h1 className="text-2xl font-bold text-center">{config.name}</h1>{" "}
         <div>
