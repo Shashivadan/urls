a7f8662f65cf6e4c04c221c99fc9d6f97a970037
Shashivadan <shashivadan99@gmail.com>
2024-08-19 14:34:37 +0530

    feat: added icons

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,3 +1,5 @@
+"use clinet";
+
 import React from "react";
 
 import { Alert } from "@/components/ui/alert";
@@ -9,8 +11,12 @@ export default function Social() {
       {config.links.map((item, index) => (
         <Alert
           key={index}
-          className="md:w-1/4 text-white border-none bg-zinc-800  "
+          className="md:w-1/4 text-white border-none bg-zinc-800 flex gap-2  "
         >
+          <div
+            dangerouslySetInnerHTML={{ __html: `<span>${item.icon}</span>` }}
+          />
+
           <a
             target="_blank"
             href={item.link}
