3a4f5b1c57a30ef3a3d26d103525dc870f226a2f
Shashivadan <shashivadan99@gmail.com>
2024-09-13 12:16:59 +0530

    feat: added static site rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+
 
 import Profile from "@/components/profile";
 import HyperLinks from "@/components/hyper-links";
 
+export const dynamic = "force-static";
 export default function Home() {
   return <>
     <div className="max-w-screen-md mx-auto p-6 h-[calc(100vh-72px)]" >
